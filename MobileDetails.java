8969b39ae93d78dfaa763d75106d3e5ded68d426
santhosh-15-R <126150932+santhosh-15-R@users.noreply.github.com>
2023-04-02 15:10:13 +0530

    Create MobileDetails.java

---
+++ MobileDetails.java
@@ -0,0 +1,24 @@
+package Weekend1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Create a class for Mobile and declare below variables with appropriate values and print the values......
+		String mobileBrandName = "Realme 8";
+		char mobileLogo = 'R';
+		short noOfMobilePiece = 1;
+		int modelNumber=22;
+		long mobileImeiNumber = 999527495644309l;
+		float mobilePrice = 16000.0f;
+		boolean isDamaged = false;
+		
+		
+		System.out.println("Brand Name :" +mobileBrandName+ "\n"  +"Logo :"  +mobileLogo+ "\n"  + "Quantity : "+noOfMobilePiece+ "\n"  + "Model :"+modelNumber+ "\n"  +"Price :"+mobilePrice+ "\n"  +"Mobile is damaged: " +isDamaged );
+		
+
+	}
+
+}

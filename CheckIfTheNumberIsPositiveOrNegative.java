e4633f2cef4a3942b2552440be73c8887f7541b4
santhosh-15-R <126150932+santhosh-15-R@users.noreply.github.com>
2023-04-02 14:11:06 +0530

    Create CheckIfTheNumberIsPositiveOrNegative.java

---
+++ CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,24 @@
+package Weekend1;
+
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+	
+	
+public static void main(String[] args) {
+	
+	int Number=35;
+	
+	if(Number>0) {
+		
+		System.out.println("The numberis Positive");
+	}else if(Number<0){
+		
+		System.out.println("The numberis Negative");
+		
+	}else{
+		System.out.println("Number is 0");
+	} 
+	
+}
+}
+	

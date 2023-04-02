246ba64d2d70370b2dbe2b3789998d33be0d9e8c
santhosh-15-R <126150932+santhosh-15-R@users.noreply.github.com>
2023-04-02 14:23:15 +0530

    Create ConvertANegativeNumberToPositiveNumber.java

---
+++ ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,22 @@
+package Weekend1;
+
+public class ConvertANegativeNumberToPositiveNumber {
+
+	
+		
+		public static void main(String[] args) {
+			
+			int Number = -40;
+			
+			if(Number<0) {
+				
+				System.out.println(Number*(-1));
+			}
+				
+			else{
+				System.out.println("Number is Not negative number");
+			} 
+			
+		
+}
+}
